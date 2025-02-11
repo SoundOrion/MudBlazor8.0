eda4b77eed5809c4901d17dea0039573fc55b2e0
hatsuyama <hatsuyama>
2025-02-11 19:47:02 +0900

    add

---
+++ Components/Models/SerfHealthStatusModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyApplication.Components.Models;
+
+public class SerfHealthStatusModel
+{
+    public string NodeName { get; set; }
+    public string CheckID { get; set; }
+    public string Type { get; set; }
+    public string Notes { get; set; }
+    public string Output { get; set; }
+}
